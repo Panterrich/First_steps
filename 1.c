bf65e14f57ab8f99819e17a7ab1f891e5779ad1f
Aleksei Durnov <durnov.an@phystech.edu>
2020-08-28 17:23:48 +0300

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
-main()
+#include <math.h>
+
+const double deviation=1E-6;
+
+int isZero(double discr)
 {
-    printf("%4d", 546863);
+	if (fabs(discr)<=deviation) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void quadratic_equation(double a, double b, double c)
+{
+	if (a=0){
+		printf("%lg", -c/b );
+	}
+	else {
+
+		double discriminant;
+
+		discriminant=b*b-4*a*c;
+
+		if (! isZero(discriminant) && (discriminant > 0) ) {
+			printf("x1=%lg\n x2=%lg\n", (-b-sqrt(discriminant))/(2*a), (-b+sqrt(discriminant))/(2*a) );
+		}
+		else {
+			if (isZero(discriminant)) {
+				printf("x=%lg\n", -b/(2*a));
+			}
+			else {
+				printf("Не имеет решений");
+			}
+		}
+	}
+ }
+void main()
+{
+	quadratic_equation(1, 2, 1);
 }
