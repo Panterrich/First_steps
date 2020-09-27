3570507983b5356ce5426e41a580bbf5131df234
Panterrich <durnov.an@phystech.edu>
2020-09-28 00:53:13 +0300

    updated

--- html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['output_11',['output',['../Solver__of__quadratic__equation_8c.html#a1e81f0a112976496bebfd8b999331173',1,'Solver_of_quadratic_equation.c']]]
-];
--- html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quadratic_5fequation_12',['quadratic_equation',['../Solver__of__quadratic__equation_8c.html#a11b51e3cb1c7402e6e2c8ca93c203f2b',1,'Solver_of_quadratic_equation.c']]]
-];
